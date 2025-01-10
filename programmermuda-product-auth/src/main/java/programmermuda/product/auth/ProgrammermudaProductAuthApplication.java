0b36206f9e7eca80a14d2a86016deac4495a662b
saipular <rizkymuu5@gmail.com>
2025-01-10 20:53:03 +0700

    first commit

---
+++ programmermuda-product-auth/src/main/java/programmermuda/product/auth/ProgrammermudaProductAuthApplication.java
@@ -0,0 +1,13 @@
+package programmermuda.product.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammermudaProductAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammermudaProductAuthApplication.class, args);
+	}
+
+}
---
+++ programmermuda-product-auth/src/test/java/programmermuda/product/auth/ProgrammermudaProductAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package programmermuda.product.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammermudaProductAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
